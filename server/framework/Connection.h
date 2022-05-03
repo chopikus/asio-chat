2c9016bca924881753f15fefb7b2cf9008ff3606
Ihor Chovpan <cigo1234r@gmail.com>
2022-05-03 13:48:17 +0200

    added connection() methods

--- server/framework/Connection.h
+++ server/framework/Connection.h
@@ -22,7 +22,7 @@ namespace MyFramework {
                 context_(asioContext), socket_(std::move(socket)),
                 qMessagesIn_(qMessagesIn) 
             {
-                ownerType = parent;
+                owner_ = parent;
             };
             
             virtual ~Connection() {
@@ -30,7 +30,7 @@ namespace MyFramework {
             }
 
             void ConnectToClient(uint32_t id) {
-                if (ownerType == Owner::SERVER) {
+                if (owner_ == Owner::SERVER) {
                     if (socket_.is_open()) {
                         connection_id = id;
                     }
@@ -56,31 +56,83 @@ namespace MyFramework {
             ThreadSafeDeque<Message<T>> qMessagesOut_;
             ThreadSafeDeque<OwnedMessage<T>>& qMessagesIn_;    
             Message<T> tempMessage_;
-            Owner ownerType = Owner::SERVER;
+            Owner owner_ = Owner::SERVER;
 
         private:
             //ASYNC
             void ReadHeader() {
                 boost::asio::async_read(socket_, tempMessage_.header, sizeof(MessageHeader<T>), 
-                [this](std::error_code ec, size_t length) {
+                [this](std::error_code ec, std::size_t length) {
                     if (!ec) {
-
+                        if (tempMessage_.header.size > 0) {
+                            tempMessage_.body.resize(tempMessage_.header.size);
+                            ReadBody();
+                        } else {
+                            AddToIncomingMessageQueue();
+                        }
                     } else {
                         std::cout << "[SERVER] [" << connection_id << "] Read Header fail!" << std::endl;
+                        socket_.close();
                     }
                 });
             }
 
+            //ASYNC
             void ReadBody() {
-
+                asio::async_read(socket_, asio::buffer(tempMessage_.body.data(), tempMessage_.body.size()),
+                [this](std::error_code ec, std::size_t length) {
+                    if (!ec) {
+                        AddToIncomingMessageQueue();
+                    } else {
+                        std::cout << "[SERVER] [" << connection_id << "] Body Read Failed." << std::endl;
+                        socket_.close();
+                    }
+                });
             }
 
+            //ASYNC
             void WriteHeader() {
-
+                asio::async_write(socket_, asio::buffer(&qMessagesOut_.front().header, sizeof(MessageHeader<T>)), 
+                [this] (std::error_code ec, std::size_t length) {
+                    if (!ec) {
+                        if (qMessagesOut_.front().body.size() > 0) {
+                            WriteBody();
+                        } else {
+                            qMessagesOut_.pop_front();
+                            if (qMessagesOut_.empty()) {
+                                WriteHeader();
+                            }
+                        }
+                    } else {
+                        std::cout << "[SERVER] [" << connection_id << "] Header Write Failed." << std::endl;
+                        socket_.close();
+                    }
+                });
             }
 
+            //ASYNC
             void WriteBody() {
-
+                asio::async_write(socket_, asio::buffer(qMessagesOut_.front().body.data(), qMessagesOut_.front().body.size()), 
+                [this] (std::error_code ec, std::size_t length) {
+                    if (!ec) {
+                        qMessagesOut_.pop_front();
+                        if (!qMessagesOut_.empty()) {
+                            WriteHeader();
+                        }
+                    } else {
+                        std::cout << "[SERVER] [" << connection_id << "] Body Write Failed." << std::endl;
+                        socket_.close();
+                    }
+                });
+            }
+            
+            void AddToIncomingMessageQueue() {
+                if (owner == Owner::SERVER) {
+                    qMessagesIn_.push_back({shared_from_this(), tempMessage_});
+                } else {
+                    qMessagesIn_.push_back({nullptr, tempMessage_});
+                }
+                ReadHeader();
             }
     };
 };
